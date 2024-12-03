68674164790f0d5237ae6f2fff9435a4f09910c8
AL4NK <alan22518prog@gmail.com>
2024-12-02 22:16:07 -0600

    snake

--- app/src/main/java/com/example/snakemath/GameView.kt
+++ app/src/main/java/com/example/snakemath/GameView.kt
@@ -177,7 +177,7 @@ class GameView @JvmOverloads constructor(
         startMovement()
 
         headBitmap = Bitmap.createScaledBitmap(
-            BitmapFactory.decodeResource(resources, R.drawable.chompa),
+            BitmapFactory.decodeResource(resources, R.drawable.chompa1),
             headwidth,
             headheight,
             true)
